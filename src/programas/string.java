fc5e09e52c57f213579cfa72a609e504abbc0457
Jhosselin <Jhosselin@192.168.18.22>
2025-06-02 21:08:33 -0500

    carga inicial

---
+++ src/programas/string.java
@@ -0,0 +1,6 @@
+
+package programas;
+
+class string {
+    
+}
---
+++ src/programas/venta.java
@@ -0,0 +1,28 @@
+package programas;
+import java.util.Scanner;
+public class Venta {
+    public static void main(String[] args) {
+        // Declarar variables
+        String vendedor;
+        double venta1, venta2, venta3, comision, sm, sn;
+        Scanner lectura = new Scanner(System.in);
+        // Entrada
+        System.out.print("Ingresar vendedor: ");
+        vendedor = lectura.nextLine();  
+        System.out.print("Ingresar sueldo mensual: ");
+        sm = lectura.nextDouble();   
+        System.out.print("Ingresar venta 1: ");
+        venta1 = lectura.nextDouble();
+        System.out.print("Ingresar venta 2: ");
+        venta2 = lectura.nextDouble();
+        System.out.print("Ingresar venta 3: ");
+        venta3 = lectura.nextDouble();
+        // Proceso
+        comision = (venta1 + venta2 + venta3) * 0.10;
+        sn = sm + comision;
+        System.out.println("La comisión de la venta es: " + comision);
+        System.out.println("El sueldo neto es: " + sn);
+    }
+}
+
+
